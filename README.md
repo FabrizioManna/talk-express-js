953d03be9051db7476f0be31d8fa2f5a7261eb6b
Fabrizio Manna <125097208+FabrizioManna@users.noreply.github.com>
2023-11-07 18:55:27 +0100

    Init project

---
+++ README.md
@@ -0,0 +1,23 @@
+# Talk ExpressJS
+
+Un semplice guestbook realizzato con ExpressJS e Bootstrap.
+
+## Avvio
+
+1. Avvia i servizi con `docker-compose up --build`.
+2. Installa le dipendenze con `npm install`.
+3. Avvia il progetto in modalità sviluppo  `npm install`.
+
+## Uso
+
+1. Accedi all'applicazione all'indirizzo `http://localhost:3000/formguest`.
+2. Compila il form con il tuo nome e messaggio.
+3. Clicca sul pulsante "Invia".
+4. Vai alla pagina `http://localhost:3000` e vedi i messaggi caricati.
+   
+## Entrypoint
+
+1. GET <http://localhost:3000/guestbook> per avere la lista json dei messaggi.
+2. POST <http://localhost:3000/guestbook> per crearne uno nuovo.
+
+## Un saluto a tutta la community
--- app.js
+++ app.js
@@ -1,19 +1,86 @@
+// Import delle dipendenze
 const express = require("express");
 const app = express();
 const path = require("path");
 const bodyParser = require("body-parser");
+const mysql = require("mysql2");
 
 
+// Connessione al DB
+const connection = mysql.createConnection({
+    host: "mysql",
+    user: "admin",
+    password: "admin",
+    database: "guestbooks"
+});
+
+// Middleware a livello di applicazione
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, resp) => {
-    resp.send("Hello World!")
+
+// Homepage
+app.get('/', (req, res) => {
+	try {
+		connection.query(
+            `SELECT * FROM messages`,
+            (error, results, fields) => {
+                if(error) throw error;
+                res.render('index', { guestbook: results });
+            }
+        );
+  	} catch (error) {
+    	next(error);
+  	}
 });
 
-app.get("*", (req, resp) => resp.status(404));
+app.get('/formguest', (req, res) => {
+	try {
+		res.render('form');
+  	} catch (error) {
+    	next(error);
+  	}
+});
+
+// Rotte
+app.get("/guestbook", (req, resp) => {
+    try {
+        connection.query(
+            `SELECT * FROM messages`,
+            (error, results, fields) => {
+                if(error) throw error;
+                resp.status(200).json(results);
+            }
+        )
+    }
+    catch(error) {
+        console.log("ERROR THROW GET GUESTBOOK:", error);
+    }
+});
+
+app.post("/guestbook", (req, resp) => {
+    try {
+        const { name, message } = req.body;
+        connection.query(
+            `INSERT INTO messages(name, message) VALUES (?,?)`,
+            [name, message],
+            (error, results, fields) => {
+                if(error) resp.status(500).json(error);
+                else {
+                    resp.status(201).json({ status: 201 });
+                }
+            }
+        )
+    
+    }
+    catch(error) {
+        console.log("ERROR THROW:", error);
+    }
+});
+
+app.get("*", (req, resp) => resp.status(404).json());
 
 app.listen(3000, (req, resp) => {
     console.log("🚀 SERVER LISTEN ON PORT: 3000");
